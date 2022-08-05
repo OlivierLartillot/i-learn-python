69732445535bec2a4a80311a2e8779c37c381599
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-08-05 10:56:04 +0200

    exercice 1 amélioré, ajoute des ingredients à ta pizza

--- 06. input-et-while.py
+++ 06. input-et-while.py
@@ -23,3 +23,41 @@ while flag == False:
         age = input('Tu dois entrer un chiffre !\nQuel age as tu ?')
         
     
+## Exercices
+
+###Exercice 1 ajoute des ingrédients (modifié ++)
+
+name = input('\nComment t appeles tu ?')
+ingredients_en_stock = ['cornichons', 'poivrons', 'tomates', 'oignons', 'champignons', 'saumon', 'olives']
+dico_aide = {
+    "terminer" : "terminer: Pour envoyer votre commande",
+    "liste" : "liste: Pour voir la liste des ingrédients disponible"
+}
+print(f'Bonjour {name.title()}.\n\n Voici la liste des ingrédients disponibles:\n {ingredients_en_stock}\n')
+liste_ingredients = []
+flag = True
+
+while flag == True:
+    if liste_ingredients:
+        print(f'Les ingérdients choisis : {liste_ingredients}')
+    ingredient = input("Quel ingrédient veux tu ajouter ?\n\tplus de commandes: aide\n")
+    ingredient = ingredient.lower()
+    if ingredient == 'terminer':
+        flag = False 
+    elif ingredient == 'aide':
+        for label,texte in dico_aide.items():
+            print(f'\t-{texte}')
+        continue
+    elif ingredient == 'liste':
+        print(f"Voici les ingrédients en stock:\n{ingredients_en_stock}")
+    elif ingredient not in ingredients_en_stock:
+        print(f"\tDésolé ! L'ingredients {ingredient} n'existe pas dans nos stock")
+        continue
+    else:
+        liste_ingredients.append(ingredient)
+
+print(f'{name.title()}, tu as choisi :')
+for ingredient in liste_ingredients:
+    print(f'- {ingredient}')
+print('Ta pizza sera prête dans 5 minutes !')  
+
